4e87d2dd57e1d2c30c499cb474e3acb337a4afb7
DiMariia <mdidenko27@gmail.com>
2022-04-11 19:14:57 +0300

    add new section carusel

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.carousel_inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="src/img/left.png" alt="slide"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="src/img/right.png" alt="slide"></button>'
+    });
+  });
