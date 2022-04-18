721c16c62080e2c733bdcfd91e85afa9f39f7847
DiMariia <mdidenko27@gmail.com>
2022-04-18 14:08:05 +0300

    add tablet adaptation

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $('.carousel_inner').slick({
-        speed: 1200,
+        speed: 1000,
         adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="src/img/left.png" alt="slide"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="src/img/right.png" alt="slide"></button>'
